0e32e76d953c7bbf3105dc2b8689a4c4bedb715e
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-01-09 14:14:26 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useReducer, useState, useEffect } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "TOGGLE":
+      return {
+        ...state,
+        showText: !state.showText,
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
+
+  const changeState = (event) => {
+    dispatch({ type: "INCREMENT" });
+    dispatch({ type: "TOGGLE" });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{state.count}</div>
+        <button onClick={(event) => changeState(event)}>Click</button>
+        {state.showText ? <div>this is a text</div> : <div></div>}
       </header>
     </div>
   );
