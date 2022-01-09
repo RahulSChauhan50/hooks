0865e4c722b8114f1a1263654583cd8c8e2e7b2b
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-01-10 00:59:19 +0530

    Shape checking in propType

--- src/Child.js
+++ src/Child.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import propTypes from "prop-types";
-export default function Child({ arrNumber, arrString }) {
+export default function Child({ person }) {
   return (
     <div>
-      <h1>{arrString + arrNumber}</h1>
+      <h1>{person.name + person.age}</h1>
     </div>
   );
 }
 
 Child.propTypes = {
-  arrNumber: propTypes.array.isRequired,
-  arrString: propTypes.arrayOf(propTypes.string).isRequired,
+  person: propTypes.shape({
+    name: propTypes.string,
+    age: propTypes.number,
+  }),
 };
