6a1e4962f7181d4dd2f28e6f77c9e6a19f2f7115
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-01-09 23:25:15 +0530

    useMemo

--- src/App.js
+++ src/App.js
@@ -7,21 +7,44 @@ import React, {
   useRef,
   useLayoutEffect,
   createContext,
+  useMemo,
 } from "react";
-import User from "./User";
-import Login from "./Login";
-
-export const AppContext = createContext(null); //used to pass props to every child in it
 
 function App() {
-  const [username, setUsername] = useState("");
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const findLongestName = (comments) => {
+    //this function return the value of longest name and is called in render method
+    // hence everytime the state changes this is called which is inefficient
+    //to avoid this we use useMemo
+    if (!comments) return null;
+    let longestName = "";
+    for (let i = 0; i < comments.length; i++) {
+      let currentName = comments[i].name;
+      if (currentName.length > longestName.length) {
+        longestName = currentName;
+      }
+    }
+    console.log("This was computed");
+    return longestName;
+  };
+  const getLongestName = useMemo(() => findLongestName(data), [data]); //accepts a function and a state on which
+  //change it recomputes the function
+
   return (
     <div className="App">
       <header className="App-header">
-        <AppContext.Provider value={{username, setUsername}}>
-          <Login />
-          <User />
-        </AppContext.Provider>
+        {/* <h1>{findLongestName(data)}</h1> */}
+        <h1>{getLongestName}</h1>
+        <button onClick={() => setToggle(!toggle)}>Toggle</button>
+        <h1>{toggle ? "Toggle" : ""}</h1>
       </header>
     </div>
   );
