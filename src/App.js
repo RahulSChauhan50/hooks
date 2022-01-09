7e6e9e5b87655af71aa3386bd993d84dad8ad613
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-01-10 00:40:00 +0530

    propType basic

--- src/App.js
+++ src/App.js
@@ -16,20 +16,10 @@ function App() {
   const [data, setData] = useState("subscribe");
   const [toggle, setToggle] = useState(false);
 
-  const returnComment = useCallback(
-    (name) => {
-      //the returnComment function is being re-created on every render which triggers the Child Component useEffect
-      //to avoid this we will use useCallback
-      return data + name;
-    },
-    [data]
-  );
   return (
     <div className="App">
       <header className="App-header">
-        <Child returnComment={returnComment} />
-        <button onClick={() => setToggle(!toggle)}>Toggle</button>
-        <h1>{toggle ? "Toggle" : ""}</h1>
+        <Child name={"sally"} age={24} />
       </header>
     </div>
   );
--- src/Child.js
+++ src/Child.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
-
-export default function Child({ returnComment }) {
-  useEffect(() => {
-    console.log("function is called");
-  }, [returnComment]); //here function is passed as prop changes and the useEffect will only be
-  // called when the return value will change
+import propTypes from "prop-types";
+export default function Child({ name, age }) {
   return (
     <div>
-      <h1>{returnComment("grey")}</h1>
+      <h1>{"in 5 years " + name + " will be " + age + 5}</h1>
     </div>
   );
 }
+
+Child.propTypes = {
+  name: propTypes.string, //throws error when datatype of prop changes
+  age: propTypes.number.isRequired, //check for missing prop
+};
